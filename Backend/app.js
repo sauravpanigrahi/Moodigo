39e15ce5719792ab11fc70fbdf4098b15e9d5035
Saurav Panigrahi <sauravpanigrahi2004@gmail.com>
2025-06-09 13:27:52 +0530

    cores connectivity issue

--- Backend/app.js
+++ Backend/app.js
@@ -82,7 +82,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodoverride("_method"));
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",           // Local development
+    "https://moodigo-web-app.web.app"  // Production frontend
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
